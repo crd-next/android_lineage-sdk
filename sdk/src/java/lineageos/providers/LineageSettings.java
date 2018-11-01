8cb371e1e903272ddecd813fa59569cdd6e22149
Pranav Vashi <neobuddy89@gmail.com>
2018-11-01 23:51:08 +0530

    Add device key action to take screenshot [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -1093,12 +1093,13 @@ public final class LineageSettings {
          * 9 - Toggle split screen
          * 10 - Kill foreground app
          * 11 - Torch
+         * 12 - Screenshot
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
         /** @hide */
         public static final Validator KEY_HOME_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the home key is double-tapped.
@@ -1109,7 +1110,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_HOME_DOUBLE_TAP_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the back key is long-pressed.
@@ -1120,7 +1121,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_BACK_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Whether to wake the screen with the back key, the value is boolean.
@@ -1169,7 +1170,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the menu key is long-pressed.
@@ -1180,7 +1181,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the assistant (search) key is pressed. (Default is 3)
@@ -1190,7 +1191,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the assistant (search) key is long-pressed. (Default is 4)
@@ -1200,7 +1201,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the app switch key is pressed. (Default is 2)
@@ -1210,7 +1211,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the app switch key is long-pressed. (Default is 0)
@@ -1220,7 +1221,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Action to perform when the screen edge is long-swiped. (Default is 0)
@@ -1230,7 +1231,7 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator KEY_EDGE_LONG_SWIPE_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 11);
+                new InclusiveIntegerRangeValidator(0, 12);
 
         /**
          * Whether to wake the screen with the home key, the value is boolean.
--- sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -37,7 +37,8 @@ public class DeviceKeysConstants {
         LAST_APP,
         SPLIT_SCREEN,
         KILL_APP,
-        TORCH;
+        TORCH,
+        SCREENSHOT;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
